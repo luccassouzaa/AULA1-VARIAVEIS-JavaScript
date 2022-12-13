700fe3ff22253c39a71fffa5d7b63e6f085205f2
Luccas Andre <luccasmelim@yahoo.com.br>
2022-12-13 18:18:26 -0300

    v2.0

---
+++ script.js
@@ -0,0 +1,35 @@
+var nome = 'Luccas Souza' //string - Texto
+var age = 18 //int - Inteiro
+var weight = 66.5 //float - Real
+var alive = false //boolean - Booleano
+var food = null //null - Nulo
+var time //undefined - Não definido
+
+var fruits = ["Banana", "Maçã", "Pera"] //Vetor - Composto
+
+var person = {
+    name: nome,
+    Idade: 18,
+    Altura: 1.80,
+    Vivo: true
+} //Objetos
+
+console.log(nome)
+console.log(age)
+console.log(weight)
+console.log(alive)
+console.log(food)
+console.log(time)
+
+console.log(fruits)
+
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+
+console.log(person.nome)
+console.log(person.Idade)
+console.log(person.Altura)
+console.log(person.Vivo)
+
+console.log(typeof nome)
